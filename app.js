37bd79c68b804f1c0e05b05e6b8a5d3ab5b853b9
Farhat Rasheed <farhat3822@gmail.com>
2024-12-30 22:16:19 -0800

    app.js

--- app.js
+++ app.js
@@ -2,3 +2,34 @@
 for(var i = 1; i <= 100 ; i++){
     document.write(i + "<br>")
 }
+
+
+//Odd numbers
+for(var i = 1; i <= 100; i = i + 2){
+    document.write(i + "<br>")
+}
+
+
+//Table 
+var num = prompt("Enter your number")
+for(var i = 1; i <= 10 ; i++){
+    document.write(num + "x" + i + "=" + num*i + "<br>")
+}
+
+//Table with limit
+var num = prompt("Enter your table number")
+var num1 = prompt("Enter your limit of table")
+var a = num1
+for(var i = 1;i <= a; i++){
+
+    document.write(num + "x" + i + "=" + num * i +  "<br>" )
+}
+
+
+//nested loop
+for(var i=1;i <=5;i++){
+    for(var j=1; j<=i; j++){
+        document.write(j)
+       }
+       document.write( "<br>")
+ }
